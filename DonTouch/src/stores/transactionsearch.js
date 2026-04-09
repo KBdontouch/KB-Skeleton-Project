456bc45e05a4ab3aa7997af573dee2b656f31f10
hajinKim <hajin1456@gmail.com>
2026-04-09 16:12:26 +0900

    Feat. 거래 내역 조회 기능 구현 (타입별 리스트 출력, 유형 메뉴바 구현)

--- DonTouch/src/stores/transactionsearch.js
+++ DonTouch/src/stores/transactionsearch.js
@@ -1,12 +1,7 @@
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import { defineStore } from 'pinia';
+import axios from 'axios';
 
-export const useCounterStore = defineStore('search', () => {
-  //data
-
-  //computed
-
-  //getters
-
-  return { count, doubleCount, increment };
+export const useSearchStore = defineStore('search', () => {
+  return {};
 });
