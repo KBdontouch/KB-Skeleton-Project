15b52c3fb813996d15fabaa923b9be6bfd5b48d8
hajinKim <hajin1456@gmail.com>
2026-04-09 13:05:41 +0900

    Feat. 거래 내역 조회 화면 구성

---
+++ DonTouch/src/stores/transactionsearch.js
@@ -0,0 +1,12 @@
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
+
+export const useCounterStore = defineStore('search', () => {
+  //data
+
+  //computed
+
+  //getters
+
+  return { count, doubleCount, increment };
+});
